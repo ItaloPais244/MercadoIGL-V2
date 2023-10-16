4a9de987d53df2d1e48f6fa7e0f199496995b423
Italo Pais <italo.pais8942@gmail.com>
2023-10-15 23:46:15 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MercadoIGL.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Id")]
+        public int idProduto { get; set; }
+
+        [Required(ErrorMessage = "Campo Fornecedor é OBRIGATORIO")]
+        [Display(Name = "Fornecedor")]
+        public int cnpjID { get; set; }
+        [ForeignKey("cnpjFornecedor")]
+        public Fornecedor fornecedor { get; set; }
+
+        [Required (ErrorMessage = "Campo descricao é OBRIGATORIO")]
+        [StringLength(50)]
+        [Display(Name = "Descricao")]
+        public string descricao { get; set; }
+
+        [Required (ErrorMessage = "Campo Valor Unitario é OBRIGATORIO")]
+        [Display(Name = "Valor")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public float valorUnitario { get; set; }
+
+        [Required (ErrorMessage = "Campo Estoque é OBRIGATORIO")]
+        [Display(Name = "Estoque")]
+        public int estoque { get; set; }
+    }
+}
---
+++ Models/Venda.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MercadoIGL.Models
+{
+    [Table("Vendas")]
+    public class Venda
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Id")]
+        public int idVenda { get; set; }
+
+        [Display(Name = "Produto")]
+        public int ProdutoID { get; set; }
+        [ForeignKey("idProduto")]
+        public Produto produto { get; set; }
+
+        [Display(Name = "Cliente")]
+        public int ClienteID { get; set; }
+        [ForeignKey("cpfCliente")]
+        public Cliente cliente { get; set; }
+
+        [Display(Name = "Funcionario")]
+        public int FuncionarioID { get; set; }
+        [ForeignKey("cpfFuncionario")]
+        public Funcionario funcionario { get; set; }
+
+        [Display(Name = "Quantidade")]
+        [Required]
+        public int QntdVendida { get; set; }
+
+        [Display(Name = "Valor Total")]
+        public float valorTotal { get; set; }
+
+        [Display(Name = "Data")]
+        public int data { get; set; }
+    }
+}
